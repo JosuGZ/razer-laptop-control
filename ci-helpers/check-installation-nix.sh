fd7e4b7b989c2ff559fcc6ceb24c769f0d7dff7f
Josu Goñi <josu_z@hotmail.com>
2024-11-07 16:14:32 +0100

    Check if the nix package has been installed

--- ci-helpers/check-installation-nix.sh
+++ ci-helpers/check-installation-nix.sh
@@ -2,6 +2,14 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
+# Check if the package was installed
+if nix-env -q razer-laptop-control; then
+  echo "razer-laptop-control package installed"
+else
+  echo "razer-laptop-control package not installed"
+  exit 1
+fi
+
 # Aparently the service is not enabled by default
 # printf "Checking that the service is enabled: "
 # systemctl --user is-enabled razercontrol.service
